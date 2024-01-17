16fed92cf8ece5b8eca7882654f89acb3ffb79a6
Soilenyis <soilenyis@gmail.com>
2024-01-17 18:05:49 -0300

    implementando refresh token

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 from src.database import get_session
 from src.models import User
 from src.schemas import Token
-from src.security import verify_password, create_access_token
+from src.security import verify_password, create_access_token, get_current_user
 
 router = APIRouter(prefix='/auth', tags=['auth'])
 
@@ -31,3 +31,10 @@ def login_for_access_token(form_data: OAuth2Form, session: Session):
     access_token = create_access_token(data={'sub': user.username})
 
     return Token(access_token=access_token, token_type='bearer')
+
+
+@router.post('/refresh_token', response_model=Token)
+def refresh_access_token(user: User = Depends(get_current_user)):
+    new_access_token= create_access_token(data={'sub': user.username})
+
+    return Token(access_token=new_access_token, token_type='bearer')
